9a2cdea6044abbbcdf8aa7ddac4302dac4a0d4f1
samanthalau1 <samanthamaili@gmail.com>
2026-01-19 20:16:29 -0800

    Modeling circular sphere object, larger time step

---
+++ simulation.m
@@ -0,0 +1,60 @@
+% creating computational grid
+Nx = 256;
+Ny = 256;
+dx = 1e-4
+dy = dx;
+
+kgrid = kWaveGrid(Nx, dx, Ny, dy);
+
+% creating homogenous medium (water)
+medium.sound_speed = 1500 * ones(Nx, Ny);
+medium.density = 1000 * ones(Nx, Ny);
+
+% create circular target
+target_radius = 10;
+target_x = round(Nx * .8);  % towards the bottom of the grid
+target_y = round(Ny / 2);   % centered
+
+target_mask = makeCircle(Nx, Ny, target_x, target_y, target_radius);
+
+% impedance mismatch for reflections
+medium.sound_speed(target_mask == 1) = 5900;
+medium.density(target_mask == 1) = 7900;
+
+% time array
+c_max = max(medium.sound_speed(:));
+t_end = 2 * Nx * dx / 1500;
+kgrid.makeTime(c_max, 0.3, t_end);
+
+% point source
+source.p_mask = zeros(Nx, Ny);
+
+source_x = 2;        % below surface
+source_y = round(Ny * .3);  % left of sensor
+source.p_mask(source_x, source_y) = 1;
+
+source_freq = 200e3;   % time varying source
+source_mag  = 5e5;
+
+source.p = source_mag * toneBurst(1/kgrid.dt, source_freq, 1);
+
+% point sensor
+sensor.mask = zeros(Nx, Ny);
+sensor_x = 2;                       % very top = surface
+sensor_y = round(Ny * .7);
+sensor.mask(sensor_x, sensor_y) = 1;
+
+sensor.record = {'p'};
+
+% running simulation
+input_args = {'PMLInside', false, 'PMLSize', 20, 'DataCast', 'single'};
+sensor_data = kspaceFirstOrder2D(kgrid, medium, source, sensor, input_args{:});
+
+% plot A-scan of point sensor
+figure;
+plot(kgrid.t_array*1e6, sensor_data.p, 'k');  % time in microseconds
+xlabel('Time [\mus]');
+ylabel('Pressure [Pa]');
+title('A-scan at Single Sensor');
+grid on;
+
