859910631014c5b8a95f6475513670511922c0b2
samanthalau1 <samanthamaili@gmail.com>
2026-01-19 21:25:21 -0800

    added metal tank + tank visualization

--- simulation.m
+++ simulation.m
@@ -1,4 +1,4 @@
-% creating computational grid
+%% creating computational grid
 Nx = 256;
 Ny = 256;
 dx = 1e-4
@@ -6,11 +6,11 @@ dy = dx;
 
 kgrid = kWaveGrid(Nx, dx, Ny, dy);
 
-% creating homogenous medium (water)
+%% creating homogenous medium (water)
 medium.sound_speed = 1500 * ones(Nx, Ny);
 medium.density = 1000 * ones(Nx, Ny);
 
-% create circular target
+%% create circular target
 target_radius = 10;
 target_x = round(Nx * .8);  % towards the bottom of the grid
 target_y = round(Ny / 2);   % centered
@@ -21,12 +21,34 @@ target_mask = makeCircle(Nx, Ny, target_x, target_y, target_radius);
 medium.sound_speed(target_mask == 1) = 5900;
 medium.density(target_mask == 1) = 7900;
 
-% time array
+%% creating metal walls
+wall_thickness = 5;
+
+% bottom wall
+bottom_wall = false(Nx, Ny);
+bottom_wall(end-wall_thickness+1:end, :) = true;
+
+% left wall
+left_wall = false(Nx, Ny);
+left_wall(:, 1:wall_thickness) = true;
+
+% right wall
+right_wall = false(Nx, Ny);
+right_wall(:, end-wall_thickness+1:end) = true;
+
+% combine all walls
+tank_walls = bottom_wall | left_wall | right_wall;
+
+% apply steel properties to walls
+medium.sound_speed(tank_walls) = 5900;
+medium.density(tank_walls) = 7900;
+
+%% time array
 c_max = max(medium.sound_speed(:));
 t_end = 2 * Nx * dx / 1500;
 kgrid.makeTime(c_max, 0.3, t_end);
 
-% point source
+%% point source
 source.p_mask = zeros(Nx, Ny);
 
 source_x = 2;        % below surface
@@ -38,7 +60,7 @@ source_mag  = 5e5;
 
 source.p = source_mag * toneBurst(1/kgrid.dt, source_freq, 1);
 
-% point sensor
+%% point sensor
 sensor.mask = zeros(Nx, Ny);
 sensor_x = 2;                       % very top = surface
 sensor_y = round(Ny * .7);
@@ -46,11 +68,17 @@ sensor.mask(sensor_x, sensor_y) = 1;
 
 sensor.record = {'p'};
 
-% running simulation
-input_args = {'PMLInside', false, 'PMLSize', 20, 'DataCast', 'single'};
+%% see tank
+figure;
+imagesc(medium.density');
+colorbar;
+title('Tank Geometry (density)');
+
+%% running simulation
+input_args = {'PMLInside', false, 'PMLSize', 40, 'DataCast', 'single'};
 sensor_data = kspaceFirstOrder2D(kgrid, medium, source, sensor, input_args{:});
 
-% plot A-scan of point sensor
+%% plot A-scan of point sensor
 figure;
 plot(kgrid.t_array*1e6, sensor_data.p, 'k');  % time in microseconds
 xlabel('Time [\mus]');
